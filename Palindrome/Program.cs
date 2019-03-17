0de0256209d801274f8d3bb68a94ef00d58fbd76
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-17 18:13:19 -0500

    final code

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,10 +6,28 @@ namespace Palindrome
     {
         public static void Main()
         {
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+            bool palindrone = IsPalindrome(word);
+            Console.WriteLine($"Is {word} a palindrome: {palindrone}");
             Console.ReadLine();
         }
+        public static bool IsPalindrome(string word)
+        {
+            word = word.ToLower();
+            char[] wordArray = word.ToCharArray();
+            Array.Reverse(wordArray);
+            string reverseWord = String.Concat(wordArray);
+            bool palindrome = false;
+            if (word == reverseWord)
+            {
+                return true;
+            }
+            return palindrome;
+        }
 
-        // TODO: Create a method that recognizes palindromes.
+
+ // TODO: Create a method that recognizes palindromes.
         // The name of the method you create should be IsPalindrome
         // The method should take a string as the input.
         // The method should return a bool - true if the input is a palindrome and false if it is not.
